0b6710271ef7f604f49c4017c5133aa9784ab3fe
pupixipup <rkbitinas@gmail.com>
2022-09-09 14:04:58 +0200

    feat: add styles

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dummyData = require('./dummy-data')
 const express = require('express')
+const path = require('path');
 const expressHandlebars = require('express-handlebars')
 
 const app = express()
@@ -8,11 +9,17 @@ app.engine("hbs", expressHandlebars.engine({
   defaultLayout: 'main.hbs'
 }))
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', function(request, response){
   const model = {
     humans: dummyData.humans
   }
   response.render("show-all-humans.hbs", model)
 })
+
+  app.get('/contacts', function(request, response){
+    response.render("about-us.hbs")
+  })
 
 app.listen(8080)
