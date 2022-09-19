1ed39c2c39fa60116a019811185d781fbb93c766
pupixipup <rkbitinas@gmail.com>
2022-09-19 17:52:03 +0200

    feat : add articles creation

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const sqlite3 = require('sqlite3');
+const express = require('express');
+const dbAPI = require('./dbAPI.js');
 const path = require('path');
-const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
-// const db = new sqlite3.Database('');
-const app = express()
+const app = express();
 
 app.engine("hbs", expressHandlebars.engine({
   defaultLayout: 'main.hbs'
 }))
 
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
+
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
 
 app.get('/about', function(request, response){
   response.render("about.hbs")
@@ -24,5 +31,28 @@ app.get('/', function(request, response){
   response.render("home.hbs")
 })
 
+
+app.get('/articles', function(request, response) {
+  dbAPI.getPosts(function(posts) {
+    console.log(posts);
+    response.render("articles.hbs", { posts });
+  });
+});
+
+app.get('/articles/create', function(request, response) {
+  response.render("create-article.hbs")
+});
+
+app.post('/articles/create', function(request, response){
+  const title = request.body.title;
+  const text = request.body.text;
+  console.log(request.body);
+  
+  dbAPI.createPost(title, text, 'url', function(){
+    response.redirect('/articles');
+  });
+});
+
 app.listen(8080)
 
+console.log("Server is running on localhost:8080")
