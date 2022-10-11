6c784db2dd578099d61ca9d41d9af0c1b46d1e75
pupixipup <114386953+romas-bokafix@users.noreply.github.com>
2022-10-11 19:08:08 +0200

    fix: change directory path

--- storage.js
+++ storage.js
@@ -1,8 +1,9 @@
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './public/uploads');
+    cb(null, path.join(__dirname, 'public', 'uploads'));
   },
   filename(req, file, cb) {
     cb(null, file.originalname);
